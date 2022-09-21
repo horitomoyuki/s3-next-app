570c528acfb97445de9d478bec43d07c00773c5e
horitomoyuki <eziltom0131@gmail.com>
2022-09-22 08:16:22 +0900

    テストで変更2

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,7 @@
 import styles from '../styles/Home.module.css';
 
 const About = () => {
-    return <div className={styles.container}>About Page　ようこそ</div>
+    return <div className={styles.container}>About Page</div>
 }
 
 export default About;
